f2b1ca3e6d9aa509423376329a9f97db9d0cf32f
Marcelo <marcelosimim@gmail.com>
2022-07-30 17:50:16 -0300

    navigating to new viewcontroller

---
+++ Deezer/Supporting Files/Protocols/Navigation.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation.swift
+//  Deezer
+//
+//  Created by Marcelo Simim Santos on 7/30/22.
+//
+
+import Foundation
+
+protocol Navigation {
+    func setupNavigationBar()
+}
+
+extension Navigation {
+    func setupNavigationBar() {}
+}
+
